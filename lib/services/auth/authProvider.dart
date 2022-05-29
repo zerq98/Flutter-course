eb453c9e7de83ea191d63114c7534f844b281e36
Mateusz Trybuła <zerq1998@gmail.com>
2022-05-29 16:23:29 +0200

    Add auth provider Add auth service

---
+++ lib/services/auth/authProvider.dart
@@ -0,0 +1,17 @@
+import 'authUser.dart';
+
+abstract class AuthProvider {
+  AuthUser? get currentUser;
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  });
+
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  });
+
+  Future<void> logOut();
+  Future<void> sendEmailVerification();
+}
